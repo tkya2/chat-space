9589bf33a5ce6a5679d5e0c8a543351d903e2c38
taku0000 <t.koyama0123@gmail.com>
2019-07-15 23:54:36 +0900

    修正
    コメントの削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -28,18 +28,13 @@ $(function(){
   }
   
 
-  $('#new_message').on('submit', function(e) {  //#new_messageは、formタグにつけたid。formがsubmitされた際、以下が行われる。
-    e.preventDefault();     // 非同期通信でメッセージの投稿を行いたいため、通常の動作を停止。/ 通常の動作： submit押される→create controllerでformの内容をDB保存→viewにredirect
+  $('#new_message').on('submit', function(e) {  
+    e.preventDefault();   
     var formData = new FormData(this);
-  // ① new FormData()でFormDataオブジェクトを新規に作成。引数にフォームの情報を入れられる。
-  // ② (this)イベントを発火させた要素、つまり#new_message、すなわちフォームのこと。
-  // ③ Mapオブジェクト:Viewから取得したformのデータは、Mapオブジェクトになっている。
-  // 「キーと値のペアのコレクション」とのことらしい。配列の中にkeyと値のペアがいくつか入ったオブジェクト、と言うふうにとりあえずようわからん。
     var url = $(this).attr('action');
     $.ajax({
       type: 'POST',
       url: url,
-      // URLを定義ルーティングみたいなもん
       data: formData,
       dataType: 'json',
       processData: false,
@@ -56,20 +51,19 @@ $(function(){
     })
     .fail(function() {
       alert('error');
-      // ajaxミスったらアラート。
     });
   });
 
   var reloadMessages = function() {
     var last_message_id = $(".message:last").data("message-id")
-    
+
     $.ajax({
       url: 'api/messages',
       type: 'get',
       dataType: 'json',
       data: {id: last_message_id}
     })
-    
+      
     .done(function(messages) {
       messages.forEach(function( message ){
         var html = message ? buildHTML(message) : ``;
